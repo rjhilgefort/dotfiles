981e7af157eee9fa8228acfbf56cd96190ea3a3a
Rob Hilgefort <rjhilgefort@gmail.com>
2018-11-15 09:31:47 -0700

    window manager experiments

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,9 +4,12 @@ hs.loadSpoon("MiroWindowsManager")
 
 hs.window.animationDuration = 0.1
 spoon.MiroWindowsManager:bindHotkeys({
-    up = {hyper, "k"},
-    right = {hyper, "l"},
-    down = {hyper, "j"},
-    left = {hyper, "h"},
-    fullscreen = {hyper, "return"}
+    up         = {hyper, "k"},
+    right      = {hyper, ";"},
+    down       = {hyper, "j"},
+    left       = {hyper, "h"},
+    fullscreen = {hyper, "return"},
+    center     = {hyper, "c"},
+    move       = {hyper, "v"},
+    resize     = {hyper, "d"}
 })
